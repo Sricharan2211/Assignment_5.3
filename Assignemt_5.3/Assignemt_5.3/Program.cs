f71e767019db9d893a1841ae3a6408499dec1e2c
Sricharan2211 <tudugani.charan@mphasis.com>
2023-11-09 15:48:48 +0530

    day 4 code 3

---
+++ Assignemt_5.3/Assignemt_5.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemt_5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPart 3: Fibonacci Sequence");
+
+            int n = 10;
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a + " ");
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            Console.ReadKey();
+        }
+    }
+}
